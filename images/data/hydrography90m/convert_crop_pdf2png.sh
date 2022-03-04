ba480b6847c05f4e2963d7b2db5b9fba5cf62d9b
selvaje74 <selv@pop-os.localdomain>
2022-03-04 18:01:06 -0500

    adding images

---
+++ images/data/hydrography90m/convert_crop_pdf2png.sh
@@ -0,0 +1,1 @@
+convert -density 600    -trim   -quality 500   -crop 640x620+0+0     /home/selv/Dropbox/Apps/Overleaf/Global_hydrographies_at_90m_resolution/figure/Fig11_plot_stream_slope.pdf  test.png
