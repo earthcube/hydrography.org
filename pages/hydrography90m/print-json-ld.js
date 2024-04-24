c03948901cb20d883104c2762911172322eb1584
Ya-Lan Yang <ylyang@illinois.edu>
2024-04-24 15:44:46 -0500

    fix resolution

--- pages/hydrography90m/print-json-ld.js
+++ pages/hydrography90m/print-json-ld.js
@@ -439,7 +439,7 @@ layers.forEach((value, key) => {
     const header = getHeader(`https://earthcube.github.io/hydrography.org/jsonld/hydrograph_layer_${lowerAndReplaceSpaces(key)}.json`,
         `Datasets for hydrography.org: ${key}`,
         `Datasets for hydrography.org: ${key}`,
-        "80 -180 -60 180")
+        "60 -150 -60 150")
 
     let dists = []
     value.forEach(([layerName, layerLink]) => {
